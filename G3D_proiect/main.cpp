7a357170078ace02ce89254eee963f3c50da20d5
stefaniaroman15 <stefania.roman@student.unitbv.ro>
2023-12-03 23:37:08 +0200

    Vertex added

--- G3D_proiect/main.cpp
+++ G3D_proiect/main.cpp
@@ -20,6 +20,37 @@ int main() {
 
     // Seteaz? contextul curent
     glfwMakeContextCurrent(window);
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    const char* fragmentShaderSource = "#version 330 core\n";
+
+        // Creare vertex shader
+        unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Creare fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Creare shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // ?terge shader-ele individuale, nu mai sunt necesare dup? ce sunt link-uite
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // Bucla principal?
     while (!glfwWindowShouldClose(window)) {
